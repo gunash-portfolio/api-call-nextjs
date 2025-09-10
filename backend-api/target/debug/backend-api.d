3377e6a51c78e0cd36dd617353ddce43bf89c971
Gunash Farzaliyev <gunashfarzaliyev@Gunashs-MacBook-Pro.local>
2025-09-10 21:58:59 +0400

    Rust backend api is created

---
+++ backend-api/target/debug/backend-api.d
@@ -0,0 +1,1 @@
+/Users/gunashfarzaliyev/nextjs+rust/backend-api/target/debug/backend-api: /Users/gunashfarzaliyev/nextjs+rust/backend-api/src/main.rs
---
+++ backend-api/target/debug/deps/backend_api-1894713af32a65d9.d
@@ -0,0 +1,5 @@
+/Users/gunashfarzaliyev/nextjs+rust/backend-api/target/debug/deps/backend_api-1894713af32a65d9.d: src/main.rs
+
+/Users/gunashfarzaliyev/nextjs+rust/backend-api/target/debug/deps/backend_api-1894713af32a65d9: src/main.rs
+
+src/main.rs:
